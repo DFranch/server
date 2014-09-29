aeb024eb63937664b2ef83648194eeec36855089
Daniel Franch <franch@podio.com>
2014-09-29 12:50:37 +0200

    .gitignore is now working

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,11 @@
 
 public class Server {
 
+    public static void main(String args[]) {
+
+    }
+
 	/*
-	I kan hente dette ned for at se om alt fungere. I kan evt. indskrive en ny kommentar her for at fors¿ge.
+	I kan hente dette ned for at se om alt fungere. I kan evt. indskrive en ny kommentar her for at forsï¿½ge.
 	*/
 }
